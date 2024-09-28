a71c387c5495130636e0e1c206f1b150ef8353b9
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-09-28 02:15:41 +0100

    fix: fixed eslint & ts errors

--- src/_components/common/admin-panel/navbar.tsx
+++ src/_components/common/admin-panel/navbar.tsx
@@ -1,14 +1,14 @@
 import { Bell, Settings } from "lucide-react";
 import { SheetMenu } from "./sheet-menu";
-import { Input } from "@/components/ui/input";
 import UserDropdown from "../header/user-dropdown";
 
-export function Navbar() {
+export function Navbar({ title }: { title: string }) {
   return (
     <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
       <div className="mx-4 sm:mx-8 flex h-14 items-center">
         <div className="flex items-center flex-1 space-x-4 lg:space-x-0 bg-green">
           <SheetMenu />
+          <p>{title}</p>
           {/* <Input placeholder="Search" className="w-full" /> */}
         </div>
         <div className="flex flex-1 gap-2 items-center space-x-2 justify-end">
